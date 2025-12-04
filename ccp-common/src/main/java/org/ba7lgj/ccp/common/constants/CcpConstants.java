267e6882bb36c138943f8a9c90289a5eb1747741
ba7lgj-dev <ba7lgj@gmail.com>
2025-12-04 22:12:05 +0800

    Add CCP modules and integrate miniapp user management

---
+++ ccp-common/src/main/java/org/ba7lgj/ccp/common/constants/CcpConstants.java
@@ -0,0 +1,15 @@
+package org.ba7lgj.ccp.common.constants;
+
+/**
+ * CCP 通用常量。
+ */
+public final class CcpConstants {
+
+    private CcpConstants() {
+    }
+
+    public static final String MODULE_PREFIX = "ccp";
+    public static final String CACHE_PREFIX = "ccp:miniuser:";
+    public static final String CLIENT_TYPE_MINIAPP = "miniapp";
+    public static final String CLIENT_TYPE_ADMIN = "admin";
+}
